e958afd39c28e0ab32bb16b41b1bf942353d3d20
intazar <intazar@smartworking.io>
2024-06-13 15:22:11 +0530

    Work on Hire Dev Part

--- template-parts/content-blocks.php
+++ template-parts/content-blocks.php
@@ -165,7 +165,7 @@
 			<div class="container mx-auto md:px-0 px-10 ready-hire-bg">
 				<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
 					
-						<div class="pl-7 pr-7 md:pl-48 md:pr-12">
+						<div class="pl-7 pr-7 md:pl-48 md:pr-12 justify-self-center self-center">
 						 <?php if(get_sub_field('main_title')): ?>
 						  <h1 class="text-center md:text-left text-3xl sm:text-4xl lg:text-4xl 3xl:text-58 font-medium text-white  md:leading-tight mt-20 mb-9"><?php echo get_sub_field('main_title'); ?></h1>
 						 <?php endif; ?>	
@@ -184,8 +184,8 @@
 						<?php $hire_dev_main_img = get_sub_field('background_image');
 						if (!empty($hire_dev_main_img) && isset($hire_dev_main_img['url'])):
 						?>
-							<div class="px-6 py-10">
-								<img src="<?php echo esc_url($hire_dev_main_img['url']); ?>" alt="<?php echo esc_attr($hire_dev_main_img['alt']); ?>">
+							<div class="px-6 py-10 flex justify-self-center">
+								<img src="<?php echo esc_url($hire_dev_main_img['url']); ?>" alt="<?php echo esc_attr($hire_dev_main_img['alt']); ?>" class="w-auto">
 							</div>
 						<?php endif; ?>
 					
