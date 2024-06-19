dfb52eb428e4a82abab6cca5f7a3f8f75a06e89f
intazar <intazar@smartworking.io>
2024-06-19 21:33:56 +0530

    work on Blog

--- template-parts/trustpilot-review.php
+++ template-parts/trustpilot-review.php
@@ -1,9 +1,11 @@
 <!-- Trustpiolot Review  -->
 <div class="w-full relative bg-dark-black grid items-center pb-28 pt-16 md:pt-36">
     <div class="container mx-auto md:px-0 px-10">
+        
         <h3 class="text-center small-intro"> Reviews </h3>
         <h1 class=" text-3xl sm:text-4xl lg:text-4xl 3xl:text-58 font-medium text-white text-center px-0 md:px-96 md:leading-tight">Our <span class="text-dark-orange"> Reviews </span></h1>
         <h3 class="text-white text-xl font-normal text-center py-6"> Read our reviews to see how our Ai-driven solutions make hiring simpler and more effective. </h3>
+        
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-14">
         <div class="trustpilot-bg mb-5">
 		  <div class="flex">
