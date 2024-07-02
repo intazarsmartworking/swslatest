e252a1b74f541c39484003f3a63c48012e086ca3
intazar <intazar@smartworking.io>
2024-07-02 22:31:28 +0530

    working on developer landing page

--- developer-landing.php
+++ developer-landing.php
@@ -5,9 +5,45 @@
  * @package sws
  */
 
-get_header();
+get_header('second');
 ?>
 
-Hello All 1234
+<?php get_template_part( 'template-parts/home-blog' );?>
+
+<!-- FAQ Section  -->
+<div class="w-full relative bg-dark-black grid items-center pb-28 pt-16 md:pt-36">
+    <div class="container mx-auto md:px-0 px-10">
+        <h3 class="text-center small-intro"> FAQ's </h3>
+        <h1 class=" text-3xl sm:text-4xl lg:text-4xl 3xl:text-58 font-medium text-white text-center px-0 md:px-96 md:leading-tight">Common questions <br><span class="text-dark-orange"> and their answers </span></h1>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-28">
+			<?php 
+				$faq_argument = array( 'post_type' => 'faq');
+				$faq_query	= new WP_Query($faq_argument);
+			?>
+			<?php while ($faq_query->have_posts()) : $faq_query->the_post();?>
+			<?php $post_id = get_the_ID(); ?>
+
+			<div>
+			  <div class="accordian-sec-bg mb-5">
+				<button class="w-full text-left py-1 px-4  focus:outline-none flex justify-between items-center" onclick="toggleAccordion(<?php echo $post_id; ?>)">
+					<span id="accordion-label-<?php echo $post_id; ?>" class="text-xl pt-6 pb-6 font-medium text-white"><?php the_title();?></span>
+					<svg id="arrow-down-<?php echo $post_id; ?>" class="h-4 w-4 text-gray-500 rounded-full" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+					  <path d="M6 9l4 4 4-4"></path>
+					</svg>
+					<svg id="arrow-up-<?php echo $post_id; ?>" class="h-4 w-4 hidden text-gray-500 rounded-full" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+					  <path d="M6 9l4-4 4 4"></path>
+					</svg>
+				</button>
+				<div id="accordion-section-<?php echo $post_id; ?>" class="overflow-hidden transition-max-height duration-300 rounded-xl" style="max-height:0;">
+				   <p class="p-4 text-base text-white"><?php echo get_the_excerpt();?></p>
+				</div>
+			  </div>
+			</div>
+			<?php endwhile; wp_reset_postdata();?>
+		</div>
+
+    </div>
+</div>
 
 <?php get_footer();?>
