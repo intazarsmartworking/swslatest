9d6eb34d8bc3ec3a87279943f9ee67a3e1da1964
Utkarsh Rathore <utkarshsinghrathore1@gmail.com>
2024-07-06 18:58:25 +0530

    testimonial UI bugs resolved

--- template-parts/client-testimonial.php
+++ template-parts/client-testimonial.php
@@ -6,126 +6,120 @@
         <span class="text-white text-[56px]">They all love us</span>
         <span class="text-dark-orange text-[56px]">What our client say</span>
     </div>
-    <div class="w-full flex relative min-h-[700px] pt-16 overflow-hidden">
-    <div class="flex w-1/12">&nbsp;</div>
-    <div class="flex w-full relative card-container">
-        <div class="bg-[#C282FA] w-[600px] h-[600px] rounded-3xl absolute card-placement card" style="--deg:-1deg; --left:10px">
+    < <div class="w-full flex relative min-h-[750px] pt-16 overflow-hidden">
+        <div class="flex w-[2%]">&nbsp;</div>
+        <div class="flex w-full relative card-container pt-10 pl-5">
+        <div id="card1" class="bg-[#C282FA] w-[600px] h-[600px] rounded-3xl absolute card-placement card"  style="--deg:-1deg; --left:10px">
             <div class="p-10 flex flex-col ">
-                <div class="flex justify-between items-center mb-6">
-                    <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
-                    <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
-                </div>
-                <p class="text-2xl text-[#050403] leading-9">
-                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
-                </p>
-                <div class="flex justify-between items-center mt-36">
-                    <div class="flex items-center">
-                        <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
-                        <div>
-                            <p class="text-[#050403] text-[24px]">Paul Simms</p>
-                            <p class="text-[#050403] text-[16px]">Tech Director</p>
-                        </div>
+                    <div class="flex justify-between items-center mb-6">
+                        <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
+                        <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
                     </div>
-                    <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    <p class="text-2xl text-[#050403] leading-9">
+                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
+                    </p>
+                    <div class="flex justify-between items-center mt-36">
+                        <div class="flex items-center">
+                            <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
+                            <div>
+                                <p class="text-[#050403] text-[24px]">Paul Simms</p>  
+                                <p class="text-[#050403] text-[16px]">Tech Director</p>  
+                            </div>
+                        </div>
+                        <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Repeat for other cards -->
-        <div class="bg-[#77BDDF] w-[600px] h-[600px] rounded-3xl absolute card-placement card" style="--deg:-0.5deg; --left:30px">
-            <!-- Content -->
+            <div id="card2" class="bg-[#77BDDF] w-[600px] h-[600px] rounded-3xl absolute card-placement card"  style="--deg:-0.5deg; --left:30px">
             <div class="p-10 flex flex-col ">
-                <div class="flex justify-between items-center mb-6">
-                    <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
-                    <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
-                </div>
-                <p class="text-2xl text-[#050403] leading-9">
-                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
-                </p>
-                <div class="flex justify-between items-center mt-36">
-                    <div class="flex items-center">
-                        <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
-                        <div>
-                            <p class="text-[#050403] text-[24px]">Paul Simms</p>
-                            <p class="text-[#050403] text-[16px]">Tech Director</p>
-                        </div>
+                    <div class="flex justify-between items-center mb-6">
+                        <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
+                        <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
                     </div>
-                    <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    <p class="text-2xl text-[#050403] leading-9">
+                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
+                    </p>
+                    <div class="flex justify-between items-center mt-36">
+                        <div class="flex items-center">
+                            <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
+                            <div>
+                                <p class="text-[#050403] text-[24px]">Paul Simms</p>  
+                                <p class="text-[#050403] text-[16px]">Tech Director</p>  
+                            </div>
+                        </div>
+                        <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="bg-[#ECC1A1] w-[600px] h-[600px] rounded-3xl absolute card-placement card" style="--deg:0deg; --left:50px">
-            <!-- Content -->
-            <div class="p-10 flex flex-col ">
-                <div class="flex justify-between items-center mb-6">
-                    <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
-                    <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
-                </div>
-                <p class="text-2xl text-[#050403] leading-9">
-                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
-                </p>
-                <div class="flex justify-between items-center mt-36">
-                    <div class="flex items-center">
-                        <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
-                        <div>
-                            <p class="text-[#050403] text-[24px]">Paul Simms</p>
-                            <p class="text-[#050403] text-[16px]">Tech Director</p>
-                        </div>
+            <div id="card3" class="bg-[#ECC1A1] w-[600px] h-[600px] rounded-3xl absolute card-placement card"  style="--deg:0deg; --left:50px">
+                <div class="p-10 flex flex-col ">
+                    <div class="flex justify-between items-center mb-6">
+                        <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
+                        <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
                     </div>
-                    <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    <p class="text-2xl text-[#050403] leading-9">
+                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
+                    </p>
+                    <div class="flex justify-between items-center mt-36">
+                        <div class="flex items-center">
+                            <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
+                            <div>
+                                <p class="text-[#050403] text-[24px]">Paul Simms</p>  
+                                <p class="text-[#050403] text-[16px]">Tech Director</p>  
+                            </div>
+                        </div>
+                        <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="bg-[#60E3D5] w-[600px] h-[600px] rounded-3xl absolute card-placement card" style="--deg:0deg; --left:47%">
-            <!-- Content -->
+            <div id="card4" class="bg-[#60E3D5] w-[600px] h-[600px] rounded-3xl absolute card-placement card"  style="--deg:0deg; --left:47%">
             <div class="p-10 flex flex-col ">
-                <div class="flex justify-between items-center mb-6">
-                    <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
-                    <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
-                </div>
-                <p class="text-2xl text-[#050403] leading-9">
-                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
-                </p>
-                <div class="flex justify-between items-center mt-36">
-                    <div class="flex items-center">
-                        <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
-                        <div>
-                            <p class="text-[#050403] text-[24px]">Paul Simms</p>
-                            <p class="text-[#050403] text-[16px]">Tech Director</p>
-                        </div>
+                    <div class="flex justify-between items-center mb-6">
+                        <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
+                        <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
                     </div>
-                    <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    <p class="text-2xl text-[#050403] leading-9">
+                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
+                    </p>
+                    <div class="flex justify-between items-center mt-36">
+                        <div class="flex items-center">
+                            <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
+                            <div>
+                                <p class="text-[#050403] text-[24px]">Paul Simms</p>  
+                                <p class="text-[#050403] text-[16px]">Tech Director</p>  
+                            </div>
+                        </div>
+                        <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="bg-[#FFFFFF] w-[600px] h-[600px] rounded-3xl absolute card-placement card" style="--deg:0deg; --left:90%">
-            <!-- Content -->
+            <div id="card5" class="bg-[#FFFFFF] w-[600px] h-[600px] rounded-3xl absolute card-placement card"  style="--deg:0deg; --left:90%">
             <div class="p-10 flex flex-col ">
-                <div class="flex justify-between items-center mb-6">
-                    <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
-                    <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
-                </div>
-                <p class="text-2xl text-[#050403] leading-9">
-                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
-                </p>
-                <div class="flex justify-between items-center mt-36">
-                    <div class="flex items-center">
-                        <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
-                        <div>
-                            <p class="text-[#050403] text-[24px]">Paul Simms</p>
-                            <p class="text-[#050403] text-[16px]">Tech Director</p>
-                        </div>
+                    <div class="flex justify-between items-center mb-6">
+                        <img src="<?php echo get_template_directory_uri();?>/images/trident_card_img.svg" />
+                        <img src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" />
                     </div>
-                    <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    <p class="text-2xl text-[#050403] leading-9">
+                    Smart Working’s process was seamless. Our goal was to find someone who would fit perfectly into our team, to deliver high value, and that’s what we got.  The feedback calls with their UK team since have been useful.  A highly recommended service.
+                    </p>
+                    <div class="flex justify-between items-center mt-36">
+                        <div class="flex items-center">
+                            <img class="mr-5" src="<?php echo get_template_directory_uri();?>/images/avatar_card_img.svg" />
+                            <div>
+                                <p class="text-[#050403] text-[24px]">Paul Simms</p>  
+                                <p class="text-[#050403] text-[16px]">Tech Director</p>  
+                            </div>
+                        </div>
+                        <img src="<?php echo get_template_directory_uri();?>/images/linkedin_card_logo.svg" />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div>
-    <div class="buttons flex gap-3">
-        <button class="text-white p-3 rounded-2xl border border-main-gray" id="prevButton">&#8592;</button>
-        <button class="text-white p-3 rounded-2xl border border-main-gray" id="nextButton">&#8594;</button>
+    <div>
+        <div class="buttons flex gap-3">
+            <button class="text-white p-3 rounded-2xl  border border-main-gray" id="prevButton">&#8592;</button>
+            <button class="text-white p-3 rounded-2xl  border border-main-gray" id="nextButton">&#8594;</button>
+        </div>
     </div>
-</div>
-
 </div>
