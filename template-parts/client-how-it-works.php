3e415105cef4652bdb4ca3ca343c36ee694338b8
intazar <intazar@smartworking.io>
2024-08-15 13:03:57 +0530

    Working on changes

--- template-parts/client-how-it-works.php
+++ template-parts/client-how-it-works.php
@@ -1,19 +1,80 @@
-<section class="relative px-0 lg:pt-28 lg:pb-16 ">
-    <div class="w-100 mb-9">
-        <h3 class="text-center small-intro"> How It Works </h3>
-        <h1 class="text-5xl sm:text-3xl lg:text-[56px] 3xl:text-58 text-center font-bold text-white mb-3">How We Find You The Best Software Talent</h1>
-        <p class="text-center text-5xl pt-[10px] sm:text-3xl lg:text-[56px] 3xl:text-58 text-dark-orange mb-3">Quick and Efficient</p>
-        <p class="text-center text-2xl pt-[10px] text-white">Select and Onboard Vetted Developers - Here is how we do it</p>
-    </div>
+<section class="relative py-16 bg-black mt-10">
+<h3 class="text-center small-intro"> How It Works </h3>
+    <h1 class="text-5xl sm:text-3xl lg:text-[56px] 3xl:text-58 text-center font-bold text-white mb-3">Hire Offshore Developers</h1>
+    <p class="text-center text-5xl pt-[10px] sm:text-3xl lg:text-[56px] 3xl:text-58 text-dark-orange mb-3">Quick Efficient & Accurate.</p>
+    <p class="text-center text-2xl pt-[10px] text-white">Select and Onboard Vetted Developers - Here is how we do it.</p>
     
-    <div class="how-it-works-slide p-3 lg:p-0" id="pinContainer">
-
-
-
-            <section class="how-it-works-card panel pt-5 white rounded-2xl bg-[#ECC1A1] lg:rounded-[44px] lg:flex lg:pt-0">
-                <div class="relative flex pl-5 pb-4 justify-between w-100 lg:w-1/6">
-                    <p class="relative  w-[100%] h-[50] lg:absolute lg:bottom-[11rem] lg:right-[7rem] howItWorkHeading gap-2">
-                        <span class="text-2 lg:text-[24px] font-extrabold">Step 1: Outline</span>
+    <div class="mt-12 w-full flex  overflow-hidden min-h-[1950px] how-it-works-section">
+        <div class="w-[5%]">&nbsp;</div>
+        <div class="w-full flex relative ">
+            <div class="how-it-works-card w-full absolute left-0 h-[800px] rounded-l-[44px] flex bg-[#ECC1A1]">
+                <div class="relative flex justify-between items-center w-1/6">
+                        <p class="absolute bottom-14 left-0 flex howItWorkHeading gap-2">
+                            <span class="text-[24px] font-extrabold">01 Meet</span>
+                        </p>
+                    </div>
+                    <div class="flex items-center justify-center">
+                        <div class="relative">
+                            <img src="<?php echo get_template_directory_uri();?>/images/howItWorksCards_avatar.svg" />
+                            <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute top-3 -left-20 rounded-3xl bg-[#F4C8BA]">
+                                <div class="flex mb-2 justify-between items-center">
+                                    <div class="flex items-center">
+                                        <span class="bg-[#C4654B] w-[40px] h-[40px] rounded-full mr-3"></span>
+                                        <p class="text-[24px] font-bold">UX Designer</p>
+                                    </div>
+                                    <p>5d</p>
+                                </div>
+                                <div class="mb-2">
+                                    <p>Part-time | New York</p>
+                                </div>
+                                <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
+                                    <p class="text-white">Applied Jan 24</p>
+                                </div>
+                            </div>
+                            <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute bottom-0 -right-20 rounded-3xl bg-[#FCEDEA]">
+                                <div class="flex mb-2 justify-between items-center">
+                                    <div class="flex items-center">
+                                        <div class="bg-[#F5C8BC] w-[40px] h-[40px] rounded-full mr-3"></div>
+                                        <p class="text-[24px] font-bold">Barista</p>
+                                    </div>
+                                    <p>5d</p>
+                                </div>
+                                <div class="mb-2">
+                                    <p>Part-time | Ohio</p>
+                                </div>
+                                <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
+                                    <p class="text-white">New</p>
+                                </div>
+                                <p>5d</p>
+                            </div>
+                            <div class="mb-2">
+                                <p>Part-time | New York</p>
+                            </div>
+                            <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
+                                <p class="text-white">Applied Jan 24</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col justify-center items-center ml-[120px] max-w-[417px]">
+                        <h3 class="text-[40px] text-bold">Meet the Candidates</h3>
+                        <ul>
+                            <li class="text-[20px] flex leading-9">
+                                <img src="<?php echo get_template_directory_uri();?>/images/bullet_point_symbol.svg" class="mr-3" />We will present the best three candidates to you.
+                            </li>
+                            <li class="text-[20px] flex leading-9">
+                                <img src="<?php echo get_template_directory_uri();?>/images/bullet_point_symbol.svg" class="mr-3" />They will match the tech stack, skills and experience you need.
+                            </li>
+                            <li class="text-[20px] flex leading-9">
+                                <img src="<?php echo get_template_directory_uri();?>/images/bullet_point_symbol.svg" class="mr-3" />Hiring made easy and simple!
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+           
+            <div class="how-it-works-card w-full absolute left-[200px] h-[800px] rounded-l-[44px] flex bg-[#77BDDF]">
+                <div class="relative flex justify-between items-center w-1/6">
+                    <p class="absolute bottom-14 left-0 flex howItWorkHeading gap-2">
+                        <span class="text-[24px] font-extrabold">02 Meet</span>
                     </p>
                 </div>
                 <div class="flex items-center justify-center">
@@ -34,11 +95,25 @@
                                 <p class="text-white">Applied Jan 24</p>
                             </div>
                         </div>
-                        
+                        <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute bottom-0 -right-20 rounded-3xl bg-[#FCEDEA]">
+                            <div class="flex mb-2 justify-between items-center">
+                                <div class="flex items-center">
+                                    <div class="bg-[#F5C8BC] w-[40px] h-[40px] rounded-full mr-3"></div>
+                                    <p class="text-[24px] font-bold">Barista</p>
+                                </div>
+                                <p>5d</p>
+                            </div>
+                            <div class="mb-2">
+                                <p>Part-time | Ohio</p>
+                            </div>
+                            <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
+                                <p class="text-white">New</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="flex flex-col justify-center items-center p-3 max-w-[100%] lg:ml-[120px] lg:pl-[0px] lg:max-w-[417px]">
-                     <h3 class="text-[40px] text-bold">Outline your tech requirements</h3>
+                <div class="flex flex-col justify-center items-center ml-[120px] max-w-[417px]">
+                    <h3 class="text-[40px] text-bold">Meet the Candidates</h3>
                     <ul>
                         <li class="text-[20px] flex items-start leading-9">
                             <img src="<?php echo get_template_directory_uri();?>/images/bullet_point_symbol.svg" class="mr-3 mt-3" />We’ll match you with the best engineers in our network & in the wider market
@@ -46,15 +121,13 @@
                         <li class="text-[20px] flex items-start leading-9">
                             <img src="<?php echo get_template_directory_uri();?>/images/bullet_point_symbol.svg" class="mr-3 mt-3" />Each candidate undergoes AI vetting, interviews, and technical tests.
                         </li>
-                        
                     </ul>
                 </div>
-            </section>
-
-            <section class="how-it-works-card panel pt-5 blue rounded-2xl bg-[#77BDDF] lg:rounded-[44px] lg:flex lg:pt-0">
-                <div class="relative flex pl-5 pb-4 justify-between w-100 lg:w-1/6">
-                    <p class="relative  w-[100%] h-[50] lg:absolute lg:bottom-[11rem] lg:right-[7rem] howItWorkHeading gap-2">
-                        <span class="text-2 lg:text-[24px] font-extrabold">Step 2: Meet</span>
+            </div>
+            <div class="how-it-works-card w-full absolute left-[400px] h-[800px] rounded-l-[44px] flex bg-[#60E3D5]">
+                <div class="relative flex justify-between items-center w-1/6">
+                    <p class="absolute bottom-20 -left-9 flex howItWorkHeading gap-2">
+                        <span class="text-[24px] font-extrabold">04 Support</span>
                     </p>
                 </div>
                 <div class="flex items-center justify-center">
@@ -106,23 +179,22 @@
                         <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute left-5 top-4 lg:top-3 lg:-left-20 rounded-3xl bg-[#F4C8BA]">
                             <div class="flex mb-2 justify-between items-center">
                                 <div class="flex items-center">
-                                    <span class="bg-[#C4654B] w-[40px] h-[40px] rounded-full mr-3"></span>
-                                    <p class="text-[24px] font-bold">UX Designer</p>
+                                    <div class="bg-[#F5C8BC] w-[40px] h-[40px] rounded-full mr-3"></div>
+                                    <p class="text-[24px] font-bold">Barista</p>
                                 </div>
                                 <p>5d</p>
                             </div>
                             <div class="mb-2">
-                                <p>Part-time | New York</p>
+                                <p>Part-time | Ohio</p>
                             </div>
                             <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
-                                <p class="text-white">Applied Jan 24</p>
+                                <p class="text-white">New</p>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
-                <div class="flex flex-col justify-center items-center p-3 max-w-[100%] lg:ml-[120px] lg:pl-[0px] lg:max-w-[417px]">
-                    <h3 class="text-[40px] text-bold">Quick & Effective Onboarding</h3>
+                <div class="flex flex-col justify-center items-center ml-[120px] max-w-[417px]">
+                    <h3 class="text-[40px] text-bold">Meet the Candidates</h3>
                     <ul>
                         <li class="text-[20px] flex items-start leading-9">
                             <img src="<?php echo get_template_directory_uri();?>/images/bullet_point_symbol.svg" class="mr-3 mt-3" />Once you’ve selected a developer we will onboard & set them up with your team.
@@ -131,7 +203,6 @@
                         <li class="text-[20px] flex items-start leading-9">
                             <img src="<?php echo get_template_directory_uri();?>/images/bullet_point_symbol.svg" class="mr-3 mt-3" />We take care of HR, Payroll, Pension and all employment costs
                         </li>
-                        
                     </ul>
                 </div>
             </section>
