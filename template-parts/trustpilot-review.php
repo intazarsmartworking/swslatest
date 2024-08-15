23f1d2274c30f9f86c79822a65ba76a8776b7cb5
intazar <intazar@smartworking.io>
2024-08-16 00:34:45 +0530

    working on review

--- template-parts/trustpilot-review.php
+++ template-parts/trustpilot-review.php
@@ -1,129 +1,68 @@
-<!-- Trustpiolot Review  -->
+<?php if (have_rows('block')) : ?>
+<?php while (have_rows('block')) : the_row(); ?>
+<?php if (get_row_layout() == 'review_section') : ?>
 <div class="w-full relative grid items-center pb-28">
     <div class="container mx-auto md:px-0 px-10">
-        
-        <h3 class="text-center small-intro"> Reviews </h3>
-        <div class=" text-5xl sm:text-3xl lg:text-[56px] 3xl:text-58 font-medium gradient-text text-center px-0 flex items-center justify-center w-full md:leading-tight"><span class="mr-[10px]">Our</span><img  src="<?php echo get_template_directory_uri();?>/images/our_trusted_review_logo.svg" alt=""/> <span class="ml-[10px]">Reviews</span> </div>
-        <h3 class="text-white text-2xl font-normal text-center py-6 px-0 "> Read our reviews to see how our Ai-driven solutions make hiring simpler and more effective. </h3>
+	
+        <?php if(get_sub_field('top_title')): ?>
+        <h3 class="text-center small-intro"> <?php echo get_sub_field('top_title'); ?> </h3>
+		<?php endif; ?>
+		
+		<?php if(get_sub_field('first_title')): ?>
+        <div class=" text-5xl sm:text-3xl lg:text-[56px] 3xl:text-58 font-medium gradient-text text-center px-0 flex items-center justify-center w-full md:leading-tight">
+		 <span class="mr-[10px]"><?php echo get_sub_field('first_title'); ?></span>
+		 <?php $title_image = get_sub_field('title_image');
+		  if (!empty($title_image) && isset($title_image['url'])):
+		 ?>
+		 
+		 <img  src="<?php echo esc_url($title_image['url']); ?>" alt="<?php echo esc_url($title_image['alt']); ?>"/> 
+		 <?php endif; ?>
+		 <span class="ml-[10px]"><?php echo get_sub_field('second_title'); ?></span> 
+		</div>
+		<?php endif; ?>
+		
+		<?php if(get_sub_field('description')): ?>
+        <h3 class="text-white text-2xl font-normal text-center py-6 px-0 "> <?php echo get_sub_field('description'); ?> </h3>
+		<?php endif; ?>
         
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-14">
+		
+		<?php if(have_rows('review_box')): ?>
+		<?php while(have_rows('review_box')): the_row(); ?>
+		<?php 
+			$name = get_sub_field('name'); 
+			$description = get_sub_field('description'); 
+		?>
         <div class="trustpilot-bg mb-5">
 		  <div class="flex">
+		  
+			  <?php $auth_image = get_sub_field('image');
+				  if (!empty($auth_image) && isset($auth_image['url'])):
+			  ?>	
 			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/paul.png" alt="">
+				<img class="mb-7 w-48" src="<?php echo esc_url($auth_image['url']); ?>" alt="<?php echo esc_url($auth_image['alt']); ?>">
 			  </div>
+			  <?php endif; ?>
 			  <div>
-				<p class="font-bold text-base text-white"> Paul </p>
-				<!-- <p class="text-xs desig"> Lead Product Designer, Google </p> -->
-                <p class="text-sm pt-5 pilot-descri"> We struggled to fill a Senior Dev-Ops role in the UK. Smartworking-solutions provided quality developers from India, handling all steps from vetting to interviews. They delivered a perfect fit. Highly recommended. </p>
+				<p class="font-bold text-base text-white"> <?php echo $name;?> </p>
+				<p class="text-sm pt-5 pilot-descri"> <?php echo $description;?> </p>
 			  </div>
 		 </div>
 		</div>
-		
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/phil.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Phil </p>
-                <p class="text-sm pt-5 pilot-descri"> Smart Working built a remote team of 6 developers for us. They quickly integrate, produce high-quality work, and operate like normal team members. We'll keep using Smart Working as we grow. </p>
-			  </div>
-		 </div>
-		</div>
-		
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/oliver.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Oliver Rebbeck </p>
-                <p class="text-sm pt-5 pilot-descri"> We've had a great experience with Smart Working. They made the process easy, and we had a new employee within weeks. Regular check-ins and guidance helped us get the best from our hire. Highly recommended. </p>
-			  </div>
-		 </div>
-		</div>
-		
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/steven.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Steven </p>
-                <p class="text-sm pt-5 pilot-descri"> Smart Working was efficient and easy to work with. They did exactly as described with no pressure or surprises, solving our development resource issues. Our developer integrated well and continues to grow. </p>
-			  </div>
-		 </div>
-		</div>
-		
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/jimmy.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Jimmy Holt </p>
-                <p class="text-sm pt-5 pilot-descri"> We hired two impressive software engineers from Smart Working. They work full-time, like regular team members, matching our hours. We're excited to have them visit the UK soon for a week-long meet-up.
-				</p>
-			  </div>
-		 </div>
-		</div>
+		<?php endwhile; ?>	
+		<?php endif; ?> 
 
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/johns.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Jones </p>
-                <p class="text-sm pt-5 pilot-descri"> Good, efficient process at Smart Working Solutions. They usually onboard new team members within a month of receiving our job description. We also feel well supported by their Indian team.
-				</p>
-			  </div>
-		 </div>
-		</div>
-
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/jermany.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Jeremy Burgess </p>
-                <p class="text-sm pt-5 pilot-descri"> Smart Working made offshore recruitment easy. They provided an excellent Senior Dev, Ritesh, who quickly integrated and impressed us. The process was fast and effective, with great support.
-				</p>
-			  </div>
-		 </div>
-		</div>
-
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/callum.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Callum Crombie </p>
-                <p class="text-sm pt-5 pilot-descri"> Smart Working made the process quick and simple. They introduced strong candidates within two days of receiving the job description. We were impressed and highly recommend their service.
-				</p>
-			  </div>
-		 </div>
+		
 		</div>
 		
-		<div class="trustpilot-bg mb-5">
-		  <div class="flex">
-			  <div class="pr-4">
-				<img class="mb-7 w-48" src="<?php echo get_template_directory_uri();?>/images/belle.png" alt="">
-			  </div>
-			  <div>
-				<p class="font-bold text-base text-white"> Belle </p>
-                <p class="text-sm pt-5 pilot-descri"> Great service from Smart Working. Vikas joined us over a year ago and has made a significant impact. Initially, we focused on communication, but now we have a great routine, and things are progressing well. </p>
-			  </div>
-		 </div>
-		</div>
-		</div>
-		
+		<?php if(get_sub_field('button_name')): ?>
 		<div class="header-buttons mt-12 text-center">
-		  <a href="https://smartworking.io/" class="button button-small  px-8 py-4 font-bold rounded-xl text-white text-lg get-started-banner-home">See all reviews</a>
+		  <a href="<?php echo get_sub_field('button_link'); ?>" class="button button-small  px-8 py-4 font-bold rounded-xl text-white text-lg get-started-banner-home"><?php echo get_sub_field('button_name'); ?></a>
 		</div>
+		<?php endif; ?> 
 
     </div>
 </div>
+<?php endif; ?>
+<?php endwhile; ?>
+<?php endif; ?>
