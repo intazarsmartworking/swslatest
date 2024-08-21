05f27b28f76dbaa4e7414f7eca62ae0483ab38e4
shaileshsmartworking <shailesh.kumar@smartworking.io>
2024-08-22 01:51:01 +0530

    remove home page slider
    remove home page slider

---
+++ contact-us.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Contact Us Page
+ *
+ * @package sws
+ */
+
+ get_header();
+?>
+
+
+<section class=" w-full py-16">
+	<div class="mx-auto w-[100%] max-w-[1280px]">
+        <div class="grid grid-cols-1 lg:grid-cols-10 gap-4 content-center relative">
+            <div class="grid-item col-span-5 relative">
+				<img class=" w-[100%] max-w-[100%] rounded-[20px]" src="<?php echo get_template_directory_uri();?>/images/smart-hire-1.jpg">
+			</div>
+            <div class="grid-item col-span-5 relative">
+				<img class=" w-[100%] max-w-[100%] rounded-[20px]" src="<?php echo get_template_directory_uri();?>/images/smart-hire-1.jpg">
+			</div>
+        </div>
+    </div>
+</section>
+
+
+
+
+<?php get_footer();?>
