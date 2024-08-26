8fad12bc679185a6500a843c824bfdb71911c9e9
intazar <intazar@smartworking.io>
2024-08-26 18:04:37 +0530

    Working on how it works

--- template-parts/client-how-it-works.php
+++ template-parts/client-how-it-works.php
@@ -9,31 +9,11 @@
     <div class="how-it-works-slide p-3 lg:p-0" id="pinContainer">
 
             <section class="how-it-works-card panel p-3 lg:-[0px] white rounded-2xl bg-[#ECC1A1] lg:rounded-[44px] lg:pt-0">
-                
                 <div class="grid grid-cols-1 lg:grid-cols-10 pt-[50px] md:pt-[0px] lg:pl-[100px] h-[100%] content-center">
                     <div class="howItWorkHeading top-[20px] lg:top-[inherit] lg:bottom-[30px] text-2 lg:text-[24px] font-extrabold">
                         Step 1: Outline
                     </div>
-                    <div class="grid-item col-span-3 relative">
-                        <div class="relative pl-[100px]">
-                            <img class="rounded-[20px] w-[100%] max-w-[100%]" src="<?php echo get_template_directory_uri();?>/images/outline-your-requirements.png" />
-                            <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute left-0 top-4 lg:top-3 rounded-3xl bg-[#F4C8BA]">
-                                <div class="flex mb-2 justify-between items-center">
-                                    <div class="flex items-center">
-                                        <span class="bg-[#C4654B] w-[40px] h-[40px] rounded-full mr-3"></span>
-                                        <p class="text-[24px] font-bold">UX Designer</p>
-                                    </div>
-                                    <p>5d</p>
-                                </div>
-                                <div class="mb-2">
-                                    <p>Part-time | New York</p>
-                                </div>
-                                <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
-                                    <p class="text-white">Applied Jan 24</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                     <div class="grid-item col-span-6 self-center lg:pl-[40px] max-w-[600px]">
                         <h3 class="text-[40px] text-bold">Outline your tech requirements</h3>
                         <ul class="hiw-bullat-point">
@@ -56,26 +36,7 @@
                     <div class="howItWorkHeading top-[20px] lg:top-[inherit] lg:bottom-[30px] text-2 lg:text-[24px] font-extrabold">
                         Step 2: Meet
                     </div>
-                    <div class="grid-item col-span-3 relative">
-                        <div class="relative pl-[100px]">
-                            <img class="rounded-[20px] w-[100%] max-w-[100%]" src="<?php echo get_template_directory_uri();?>/images/Meet-the-best-engineers.png" />
-                            <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute left-0 top-4 lg:top-3 rounded-3xl bg-[#F4C8BA]">
-                                <div class="flex mb-2 justify-between items-center">
-                                    <div class="flex items-center">
-                                        <span class="bg-[#C4654B] w-[40px] h-[40px] rounded-full mr-3"></span>
-                                        <p class="text-[24px] font-bold">UX Designer</p>
-                                    </div>
-                                    <p>5d</p>
-                                </div>
-                                <div class="mb-2">
-                                    <p>Part-time | New York</p>
-                                </div>
-                                <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
-                                    <p class="text-white">Applied Jan 24</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                     <div class="grid-item col-span-6 self-center lg:pl-[40px] max-w-[600px]">
                         <h3 class="text-[40px] text-bold">Meet the Best Engineers</h3>
                         <ul class="hiw-bullat-point">
@@ -102,27 +63,7 @@
                     <div class="howItWorkHeading top-[20px] lg:top-[inherit] lg:bottom-[30px] text-2 lg:text-[24px] font-extrabold">
                         Step 3: Onboarding
                     </div>
-                    <div class="grid-item col-span-3 relative">
-                        <div class="relative pl-[100px]">
-                            <img class="rounded-[20px] w-[100%] max-w-[100%]" src="<?php echo get_template_directory_uri();?>/images/Quick-Effective-Onboarding.png" />
-                            <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute left-0 top-4 lg:top-3 rounded-3xl bg-[#F4C8BA]">
-                                <div class="flex mb-2 justify-between items-center">
-                                    <div class="flex items-center">
-                                        <span class="bg-[#C4654B] w-[40px] h-[40px] rounded-full mr-3"></span>
-                                        <p class="text-[24px] font-bold">UX Designer</p>
-                                    </div>
-                                    <p>5d</p>
-                                </div>
-                                <div class="mb-2">
-                                    <p>Part-time | New York</p>
-                                </div>
-                                <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
-                                    <p class="text-white">Applied Jan 24</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                    </div>
+                    
                     <div class="grid-item col-span-6 self-center lg:pl-[40px] max-w-[600px]">
                         <h3 class="text-[40px] text-bold">Quick & Effective Onboarding</h3>
                         <ul class="hiw-bullat-point">
@@ -146,27 +87,7 @@
                     <div class="howItWorkHeading top-[20px] lg:top-[inherit] lg:bottom-[30px] text-2 lg:text-[24px] font-extrabold">
                      Step 4: Support
                     </div>
-                    <div class="grid-item col-span-3 relative">
-                        <div class="relative pl-[100px]">
-                            <img class="rounded-[20px] w-[100%] max-w-[100%]" src="<?php echo get_template_directory_uri();?>/images/Long-Term-support.png" />
-                            <div class="min-h-[150px] p-4 flex-col min-w-[280px] absolute left-0 top-4 lg:top-3 rounded-3xl bg-[#F4C8BA]">
-                                <div class="flex mb-2 justify-between items-center">
-                                    <div class="flex items-center">
-                                        <span class="bg-[#C4654B] w-[40px] h-[40px] rounded-full mr-3"></span>
-                                        <p class="text-[24px] font-bold">UX Designer</p>
-                                    </div>
-                                    <p>5d</p>
-                                </div>
-                                <div class="mb-2">
-                                    <p>Part-time | New York</p>
-                                </div>
-                                <div class="bg-[#DD572C] py-2 px-5 rounded-full w-max">
-                                    <p class="text-white">Applied Jan 24</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                    </div>
+                    
                     <div class="grid-item col-span-6 self-center lg:pl-[40px] max-w-[600px]">
                         <h3 class="text-[40px] text-bold">Long-term support</h3>
                         <ul class="hiw-bullat-point">
@@ -182,11 +103,5 @@
                     </div>
                 </div>
             </section>
-
-    
-    </div>
-
-
-
-
+	</div>
 </section>
