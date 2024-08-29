a3c83e73356e0c545882bec5a673f1b17b94c004
shaileshsmartworking <shailesh.kumar@smartworking.io>
2024-08-29 23:14:39 +0530

    change single hire page
    change single hire page

--- find-a-developer.php
+++ find-a-developer.php
@@ -8,10 +8,10 @@
  get_header('second');
 ?>
 
-<section class="w-full p-4 py-[100px] bg-[#050403]">
-	<div class="mx-auto w-[100%] max-w-[1280px]">
-        <div class="grid grid-cols-1 h-[100%] min-h-[700px] relative">
-            <div class="grid-item col-span-5 content-center relative">
+<section class="w-full p-0 bg-[#050403]">
+	<div class="mx-auto w-[100%]">
+        <div class="grid grid-cols-1 h-[100%] min-h-[100vh] relative">
+            <div class="grid-item col-span-5 content-center h-[100%] relative">
                 <div data-tf-live="01J58EAYDVAPF6YGY9TD4V2GEH"></div><script src="//embed.typeform.com/next/embed.js"></script>
             </div>
         </div>
