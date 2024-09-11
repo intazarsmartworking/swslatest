46373c584b7cf6df329288cfc693ad94ec4805e4
shaileshsmartworking <shailesh.kumar@smartworking.io>
2024-09-11 06:57:30 +0530

    remove text
    remove text

--- single-hire.php
+++ single-hire.php
@@ -595,7 +595,7 @@
         <div class="bg-white ml-0 lg:ml-[12%] relative rounded-3xl px-6 py-6 w-[100%] max-w-[538px]">
 			<div class="w-full">
 				<iframe aria-label='Share Your Requirement' frameborder="0" style="height:320px;width:100%;border:none;" src='https://forms.zohopublic.in/shubhankit/form/SkillPagesForm/formperma/Kg0mSBrC00V3ydnzP2AdZWY3HYv-yjP5y0vJV2CTa3E'></iframe>
-				<h4 class="text-center text-dark-black text-sm font-semibold py-6"> Our recruitment specialist will be in touch soon! </h4>
+				<!-- <h4 class="text-center text-dark-black text-sm font-semibold py-6"> Our recruitment specialist will be in touch soon! </h4> -->
 			</div>
        </div>
       </div>
