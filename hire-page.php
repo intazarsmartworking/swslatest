e5d2c2beb15012dcc952b1780fe23bc4dd649ef2
intazar <intazar@smartworking.io>
2024-10-01 22:11:30 +0530

    change done

--- hire-page.php
+++ hire-page.php
@@ -20,7 +20,7 @@
                     <div class="col-span-2 md:col-span-1 content-center">
 					<?php if(get_sub_field('main_heading')): ?>
                         <h1 class="font-medium text-[35px] md:text-[56px] mb-4 text-white  lg:leading-[74px]">
-                                <?php echo get_sub_field('main_heading'); ?><br><span class="gradient-text"> <?php echo get_sub_field('orange_heading'); ?> </span> <br><span class="text-dark-orange"> <?php echo get_sub_field('orange_heading'); ?> </span> 
+                                <?php echo get_sub_field('main_heading'); ?><br><span class="gradient-text"> <?php echo get_sub_field('gradient_heading'); ?> </span> <br><span class="text-dark-orange"> <?php echo get_sub_field('orange_heading'); ?> </span> 
                         </h1>
 					<?php endif; ?>	
 					<?php if(get_sub_field('paragraph')): ?>
