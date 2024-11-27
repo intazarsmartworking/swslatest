a02f8dae3e386813766a37c9ead886d451643e41
intazar <intazar@smartworking.io>
2024-11-27 20:46:58 +0530

    multistep

--- multi-step-form.php
+++ multi-step-form.php
@@ -12,7 +12,7 @@
 	<div class="mx-auto w-[100%]">
         <div class="grid grid-cols-1 h-[100%] min-h-[100vh] relative">
             <div class="grid-item col-span-5 content-center h-[100%] relative">
-                <?php echo do_shortcode('[contact-form-7 id="300e693" title="Multi Step"]');?>
+                <?php echo do_shortcode('[contact-form-7 id="b390bcb" title="Multi Step"]');?>
             </div>
         </div>
     </div>
