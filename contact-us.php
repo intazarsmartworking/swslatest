e8070f2eb4b3304a1e70c43d2e13eae8f0cfa7e0
shaileshsmartworking <shailesh.kumar@smartworking.io>
2024-12-03 21:52:47 +0530

    conflict resolve
    conflict resolve

--- contact-us.php
+++ contact-us.php
@@ -67,21 +67,28 @@
 		<?php endif; ?>
 		<?php endwhile; ?>
 		<?php endif; ?>	
+
+        <div class="grid-item col-span-7 content-center relative">
+            <div class="calendly-inline-widget" data-url="https://calendly.com/smart-working/discovery-call-smartworking?primary_color=ff4d04" style="min-width:320px;height:700px;"></div>
+            <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+        </div>
 		
 
-		<?php if (have_rows('contact_block')) : ?>
+		<!-- <?php if (have_rows('contact_block')) : ?>
 		<?php while (have_rows('contact_block')) : the_row(); ?>
 		<?php if (get_row_layout() == 'form_section') : ?>
-		<div class="grid-item col-span-5 content-center relative">
-		<?php if(get_sub_field('form_shortcode')): ?>
-			<div class=" bg-white rounded-[30px] min-h-[580px] py-[30px] px-[25px]">
-              <?php echo get_sub_field('form_shortcode'); ?>
+		<div class="grid-item col-span-7 content-center relative">
+
+		<?php // if(get_sub_field('form_shortcode')): ?>
+			<div class="w-full">
+
+              <?php // echo get_sub_field('form_shortcode'); ?>
             </div>
-		<?php endif; ?> 	
+		<?php // endif; ?> 	
 		</div>
 		<?php endif; ?>
 		<?php endwhile; ?>
-		<?php endif; ?>	
+		<?php endif; ?>	 -->
         </div>
     </div>
 </section>
