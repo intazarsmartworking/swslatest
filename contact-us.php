6b83b489cf175d4b179fe0bde54bdea702781a7d
shaileshsmartworking <shailesh.kumar@smartworking.io>
2024-12-03 21:29:56 +0530

    add calendly link on contact form
    add calendly link on contact form

--- contact-us.php
+++ contact-us.php
@@ -8,12 +8,12 @@
  get_header('second');
 ?>
 <section class="w-full p-4 py-[30px]">
-	<div class="mx-auto w-[100%] max-w-[1280px]">
+	<div class="container">
         <div class="grid grid-cols-1 lg:grid-cols-10 gap-4 h-[100%]  relative">
 		<?php if (have_rows('contact_block')) : ?>
 		<?php while (have_rows('contact_block')) : the_row(); ?>
 		<?php if (get_row_layout() == 'left_section') : ?>
-            <div class="grid-item col-span-5 content-center relative">
+            <div class="grid-item col-span-3 content-center relative">
 			
 			<?php $image = get_sub_field('image');
 				if (!empty($image) && isset($image['url'])):
@@ -68,9 +68,13 @@
 		<?php endwhile; ?>
 		<?php endif; ?>	
 			
-		<div class="grid-item col-span-5 content-center relative">
-			<div class=" bg-white rounded-[30px] min-h-[580px] py-[30px] px-[25px]">
-              <iframe aria-label='Share Your Requirement' frameborder="0" style="height:580px;width:100%;border:none;" src='https://forms.smartworking.io/shubhankit/form/ContactUsForm/formperma/id4WgG4ZljDvVm388-5p-A9d9t9TFE8-Mm8cXswDscI'></iframe>
+		<div class="grid-item col-span-7 content-center relative">
+			<div class="w-full">
+                <!-- Calendly inline widget begin -->
+                <div class="calendly-inline-widget" data-url="https://calendly.com/smart-working/discovery-call-smartworking?primary_color=ff4d04" style="min-width:320px;height:700px;"></div>
+                <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+                <!-- Calendly inline widget end -->
+              <!-- <iframe aria-label='Share Your Requirement' frameborder="0" style="height:580px;width:100%;border:none;" src='https://forms.smartworking.io/shubhankit/form/ContactUsForm/formperma/id4WgG4ZljDvVm388-5p-A9d9t9TFE8-Mm8cXswDscI'></iframe> -->
             </div>
 		</div>
         </div>
