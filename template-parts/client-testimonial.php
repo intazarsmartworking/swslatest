464429d39e0093ccd871e3386cc113b8ca181ea6
shaileshsmartworking <shailesh.kumar@smartworking.io>
2025-01-08 23:46:15 +0530

    feedback issue fix
    feedback issue fix

--- template-parts/client-testimonial.php
+++ template-parts/client-testimonial.php
@@ -1,9 +1,9 @@
 <?php if (get_row_layout() == 'testimonial_section') : ?>
-	<div class="w-full testimonial-section">
+	<div class="w-full testimonial-section p-[1rem] lg:py-[3.75rem]">
 	    
 		
 		<?php if(get_sub_field('main_heading')): ?>
-			<div class="block mt-[5rem]">
+			<div class="block">
 				<h2 class="text-[2.25rem] font-[600] text-center text-[#E8E8E8] mb-[1.25rem]"><?php echo get_sub_field('main_heading'); ?></h2>
 				<p class="text-[1.5rem] font-[600] text-center text-[#E8E8E8] mb-[1rem]">Excellent &nbsp;<img class="inline h-[1.5rem] align-middle mb-1 " src="<?php echo get_template_directory_uri();?>/images/rating_card_img.svg" alt=""></p>
 				<p class="text-[1rem] font-[600] text-center text-[#E8E8E8]">Rated 4.7/5 based on 272,822 reviews on &nbsp;<img class="inline h-[1.5rem] align-text-bottom" src="<?php echo get_template_directory_uri();?>/images/trustpilot.svg" alt=""></p>
@@ -18,7 +18,7 @@
 			</div>
 		</div>
 		<div class="w-full mt-2 testimonial-content active" id="testimonial1">
-			<div class="container relative overflow-hidden min-h-[26rem] py-8">
+			<div class="container relative overflow-hidden min-h-[26rem] pt-8">
 				<div class="slider-wrapper w-full min-h-[25rem] relative card-container overflow-visible">
 					<div class="slider-overlay">
 						<?php if(have_rows('testimonial_card')): ?>
@@ -58,14 +58,24 @@
 						
 					</div>
 				</div>
-				<div class="buttons flex justify-center gap-3 mt-3">
-					<button class="text-white w-[3rem] py-[0.2rem] text-[1.5rem] font-[700] rounded-[0.36rem] border border-[#464444] disabled:opacity-[0.3]" disabled id="nextButton">&#8592;</button>
-					<button class="text-white w-[3rem] py-[0.2rem] text-[1.5rem] font-[700] rounded-[0.36rem] border border-[#464444] disabled:opacity-[0.3]" id="prevButton" >&#8594;</button>
+				<div class="buttons flex justify-center gap-3 mt-8">
+					<button class="text-white p-[0px] disabled:opacity-[0.3]" disabled id="nextButton">
+						<svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
+							<path d="M21.9474 39C12.2581 39 4.40349 31.165 4.40349 21.5C4.40349 11.835 12.2581 4 21.9474 4C31.6366 4 39.4912 11.835 39.4912 21.5C39.4912 31.165 31.6366 39 21.9474 39Z" stroke="white" stroke-width="2.63158" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M24.1572 27.6773L17.9818 21.4998L24.1572 15.3223" stroke="white" stroke-width="2.63158" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</button>
+					<button class="text-white p-[0px] disabled:opacity-[0.3]" id="prevButton" >
+						<svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
+							<path d="M21.0517 39C30.7409 39 38.5955 31.165 38.5955 21.5C38.5955 11.835 30.7409 4 21.0517 4C11.3625 4 3.50781 11.835 3.50781 21.5C3.50781 31.165 11.3625 39 21.0517 39Z" stroke="white" stroke-width="2.63158" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M18.8428 27.6775L25.0182 21.5L18.8428 15.3225" stroke="white" stroke-width="2.63158" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</button>
 				</div>
 			</div>
 		</div>
 		<div class="w-full mt-2 testimonial-content" id="reviews1">
-			<div class="container relative overflow-hidden min-h-[26rem] py-8">
+			<div class="container relative overflow-hidden min-h-[26rem] pt-8">
 					<div class="slider-wrapper w-full min-h-[25rem] relative card-container overflow-visible">
 						<div class="slider-overlay">
 						<?php if(have_rows('testimonial_review')): ?>
@@ -105,9 +115,19 @@
 						
 					</div>
 				</div>
-				<div class="buttons flex justify-center gap-3 mt-3">
-					<button class="text-white w-[3rem] py-[0.2rem] text-[1.5rem] font-[700] rounded-[0.36rem] border border-[#464444] disabled:opacity-[0.3]" disabled id="nextButton2">&#8592;</button>
-					<button class="text-white w-[3rem] py-[0.2rem] text-[1.5rem] font-[700] rounded-[0.36rem] border border-[#464444] disabled:opacity-[0.3]" id="prevButton2" >&#8594;</button>
+				<div class="buttons flex justify-center gap-3 mt-8">
+					<button class="text-white p-[0px] disabled:opacity-[0.3]" disabled id="nextButton2">
+						<svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
+							<path d="M21.9474 39C12.2581 39 4.40349 31.165 4.40349 21.5C4.40349 11.835 12.2581 4 21.9474 4C31.6366 4 39.4912 11.835 39.4912 21.5C39.4912 31.165 31.6366 39 21.9474 39Z" stroke="white" stroke-width="2.63158" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M24.1572 27.6773L17.9818 21.4998L24.1572 15.3223" stroke="white" stroke-width="2.63158" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</button>
+					<button class="text-white p-[0px] disabled:opacity-[0.3]" id="prevButton2" >
+						<svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
+							<path d="M21.0517 39C30.7409 39 38.5955 31.165 38.5955 21.5C38.5955 11.835 30.7409 4 21.0517 4C11.3625 4 3.50781 11.835 3.50781 21.5C3.50781 31.165 11.3625 39 21.0517 39Z" stroke="white" stroke-width="2.63158" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M18.8428 27.6775L25.0182 21.5L18.8428 15.3225" stroke="white" stroke-width="2.63158" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+					</button>
 				</div>
 			</div>
 		</div>
